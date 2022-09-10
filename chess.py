d5c57cb188ba7c8507390359f2e36c6e8c51f3a2
Anish <anishrangan01@gmail.com>
2022-09-10 20:11:54 +0530

    third commit

--- chess.py
+++ chess.py
@@ -1,3 +1,6 @@
+from posixpath import split
+
+
 print("------------welocome to chess game------------")
 print("press e to exit the game")
 
@@ -27,6 +30,7 @@ while True:
     flag = True   
     p1Move = input("player 1 move: ")
     if p1Move == "e":
+        print("thank you")
         break
     p1MoveDetail = p1Move.split(":")
     for i in range(0, 5):
@@ -35,33 +39,97 @@ while True:
         for j in range(0, 5):
             if(board[i][j] == "A-"+p1MoveDetail[0]):
                 if(p1MoveDetail[1] == "F"):
-                    board[i][j] = "-   "
-                    board[i-1][j] = "A-"+p1MoveDetail[0]
-                    printBoard()
+                    if i-1 >= 0 and board[i-1][j].split("-")[0] != "A":
+                        board[i][j] = "-   "
+                        board[i-1][j] = "A-"+p1MoveDetail[0]
+                        printBoard()
+                    else:
+                        print("invalid input")
                     flag = False
                     break
                 elif(p1MoveDetail[1] == "L"):
-                    board[i][j] = "-   "
-                    board[i][j-1] = "A-"+p1MoveDetail[0]
-                    printBoard()
+                    if j-1 >= 0 and board[i][j-1].split("-")[0] != "A":
+                        board[i][j] = "-   "
+                        board[i][j-1] = "A-"+p1MoveDetail[0]
+                        printBoard()
+                    else:
+                        print("Invalid input")
                     flag = False
                     break
                 elif(p1MoveDetail[1] == "R"):
-                    board[i][j] = "-   "
-                    board[i][j+1] = "A-"+p1MoveDetail[0]
-                    printBoard()
+                    if j+1 < 5 and board[i][j+1].split("-")[0] != "A":
+                        board[i][j] = "-   "
+                        board[i][j+1] = "A-"+p1MoveDetail[0]
+                        printBoard()
+                    else:
+                        print("Invalid input")
                     flag = False
                     break
                 elif(p1MoveDetail[1] == "B"):
-                    printBoard()
-                else:
-                    board[i][j] = "-   "
-                    board[i+1][j] = "A-"+p1MoveDetail[0]
-                    printBoard()
+                    if i+1 < 5 and board[i+1][j].split("-")[0] != "A":
+                        board[i][j] = "-   "
+                        board[i+1][j] = "A-"+p1MoveDetail[0]
+                        printBoard()
+                    else:
+                        print("Invalid input")
                     flag = False
                     break
+                else:
+                    print("Invalid input")
                 break
     
+    flag = True
     p2Move = input("player 2 move: ")
     if p2Move == "e":
+        print("thank you")
         break
+    p2MoveDetail = p2Move.split(":")
+    for i in range(0, 5):
+        if flag == False:
+            break
+        for j in range(0, 5):
+            if(board[i][j] == "B-"+p2MoveDetail[0]):
+                if(p2MoveDetail[1] == "F"):
+                    if i+1 < 5 and board[i+1][j].split("-")[0] != "B":
+                        board[i][j] = "-   "
+                        board[i+1][j] = "B-"+p2MoveDetail[0]
+                        printBoard()
+                        
+                    else:
+                        print("Invalid input")
+                    flag = False
+                    break
+                    
+                elif(p2MoveDetail[1] == "L"):
+                    if j+1 < 5 and board[i][j+1].split("-")[0] != "B":
+                        board[i][j] = "-   "
+                        board[i][j+1] = "B-"+p2MoveDetail[0]
+                        printBoard()
+                        flag = False
+                    else:
+                        print("Invalid input")
+                    break
+                    
+                elif(p2MoveDetail[1] == "R"):
+                    if j-1 >= 0 and board[i][j-1].split("-")[0] != "B":
+                        board[i][j] = "-   "
+                        board[i][j-1] = "B-"+p2MoveDetail[0]
+                        printBoard()
+                        
+                    else:
+                        print("Invalid input")
+                    flag = False
+                    break
+                elif(p2MoveDetail[1] == "B"):
+                    if i-1 >= 0 and board[i-1][j].split("-")[0] != "B":
+                        board[i][j] = "-   "
+                        board[i-1][j] = "B-"+p2MoveDetail[0]
+                        printBoard()
+                        
+                    else:
+                        print("invalid input")
+                    flag = False
+                    break
+                else:
+                    print("Invalid input")
+                break
